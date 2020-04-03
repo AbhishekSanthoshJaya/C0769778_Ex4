9e86dfda3a69b67faccae5ef0ab5412ac9904b6e
AbhishekSanthoshJaya <AbhishekSanthoshJaya@users.noreply.github.com>
2020-04-03 14:39:28 -0400

    Fixed clearing all fields, all fields added

--- app/src/main/java/com/aby/c0769778_ex4/MainActivity.java
+++ app/src/main/java/com/aby/c0769778_ex4/MainActivity.java
@@ -1,28 +1,54 @@
 package com.aby.c0769778_ex4;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+import androidx.core.graphics.drawable.DrawableCompat;
 
 import android.app.DatePickerDialog;
 import android.graphics.Color;
+import android.graphics.PorterDuff;
 import android.graphics.drawable.ColorDrawable;
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.LayerDrawable;
+import android.os.Build;
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
+import android.view.Display;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.RatingBar;
 import android.widget.Scroller;
 import android.widget.Spinner;
 import android.widget.TextView;
+
+import com.google.android.material.textfield.TextInputEditText;
+import com.google.android.material.textfield.TextInputLayout;
 
 import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 private Spinner spnTitle;
 private Spinner spnDesignation;
+
 private TextView DisplayDate;
+private TextInputLayout edtFirstName;
+private TextInputLayout edtLastName;
+private TextInputLayout  edtStreetNo;
+private TextInputLayout  edtStreetName;
+private TextInputLayout  edtProvince;
+private TextInputLayout  edtPostalCode;
+private TextInputLayout  edtCountry;
+private TextInputLayout  edtNumber;
+private EditText  edtIssueDetails;
+
 private DatePickerDialog.OnDateSetListener mDateSetListener;
-private EditText edtIssueDetails;
+
+private RatingBar ratingBarSevere;
+private Button btnClear;
+private Button btnSubmit;
 
 
     @Override
@@ -31,8 +57,54 @@ private EditText edtIssueDetails;
         setContentView(R.layout.activity_main);
         spnTitle = findViewById(R.id.spnTitle);
         spnDesignation = findViewById(R.id.spnDesignation);
+
         DisplayDate = findViewById(R.id.txtDate);
         edtIssueDetails = findViewById(R.id.edtIssueDetails);
+        edtFirstName = findViewById(R.id.edtFirstName);
+        edtLastName = findViewById(R.id.edtLastName);
+        edtStreetNo = findViewById(R.id.edtStreetNo);
+        edtStreetName = findViewById(R.id.edtStreetName);
+        edtProvince = findViewById(R.id.edtProvince);
+        edtPostalCode = findViewById(R.id.edtPostalCode);
+        edtCountry = findViewById(R.id.edtCountry);
+        edtNumber = findViewById(R.id.edtNumber);
+
+        ratingBarSevere = findViewById(R.id.ratingBarSevere);
+        btnClear = findViewById(R.id.btnClear);
+        btnSubmit = findViewById(R.id.btnSubmit);
+
+        btnClear.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Clearing Spinners
+                ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(MainActivity.this,
+                        R.array.title_array, android.R.layout.simple_spinner_item);
+                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                spnTitle.setAdapter(adapter);
+
+                ArrayAdapter<CharSequence> adapter2 = ArrayAdapter.createFromResource(MainActivity.this,
+                        R.array.designation_array, android.R.layout.simple_spinner_item);
+                adapter2.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                spnDesignation.setAdapter(adapter2);
+
+                //Clearing EditText fields
+                DisplayDate.setText("");
+                edtIssueDetails.setText("");
+                edtFirstName.getEditText().setText("");
+                edtLastName.getEditText().setText("");
+                edtStreetNo.getEditText().setText("");
+                edtStreetName.getEditText().setText("");
+                edtProvince.getEditText().setText("");
+                edtPostalCode.getEditText().setText("");
+                edtCountry.getEditText().setText("");
+                edtNumber.getEditText().setText("");
+            }
+        });
+
+        LayerDrawable layerDrawable = (LayerDrawable) ratingBarSevere.getProgressDrawable();
+        DrawableCompat.setTint(DrawableCompat.wrap(layerDrawable.getDrawable(0)), Color.RED);
+        DrawableCompat.setTint(DrawableCompat.wrap(layerDrawable.getDrawable(1)), Color.GREEN);
+        DrawableCompat.setTint(DrawableCompat.wrap(layerDrawable.getDrawable(2)), Color.BLUE);
 
         edtIssueDetails.setScroller(new Scroller(MainActivity.this));
         edtIssueDetails.setMaxLines(3);
