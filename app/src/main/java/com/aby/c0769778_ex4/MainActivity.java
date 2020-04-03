4aeea0244af714bdd17212cf11b2910fb6216068
AbhishekSanthoshJaya <AbhishekSanthoshJaya@users.noreply.github.com>
2020-04-02 20:48:13 -0400

    Initial commit

---
+++ app/src/main/java/com/aby/c0769778_ex4/MainActivity.java
@@ -0,0 +1,24 @@
+package com.aby.c0769778_ex4;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+public class MainActivity extends AppCompatActivity {
+private Spinner spnTitle;
+private Spinner spnDesignation;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        spnTitle = findViewById(R.id.spnTitle);
+        spnDesignation = findViewById(R.id.spnDesignation);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.title_array, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spnTitle.setAdapter(adapter);
+    }
+}
